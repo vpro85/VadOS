60b6ffdd3ac963b2cdee4e35273ed11747addec7
vpro85 <vpro85@gmail.com>
2026-05-13 23:13:34 +0200

    _start

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,21 @@
 #![no_main]
 
 use core::panic::PanicInfo;
+use limine::request::BootloaderInfoRequest;
+use limine::BaseRevision;
+
+// Говорим Limine, что поддерживаем протокол версии 3
+#[used]
+static BASE_REVISION: BaseRevision = BaseRevision::new();
+
+// Запрашиваем информацию о загрузчике
+#[used]
+static BOOTLOADER_INFO: BootloaderInfoRequest = BootloaderInfoRequest::new();
+
+#[unsafe(no_mangle)]
+extern "C" fn _start() -> ! {
+    loop {}
+}
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
