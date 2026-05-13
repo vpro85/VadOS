2fad14572d22a2816d9fb76b9294079dea32f264
vpro85 <vpro85@gmail.com>
2026-05-13 23:43:02 +0200

    run.sh

--- run.sh
+++ run.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 set -e
 
-# Собираем ядро
 cargo build
-
-# Копируем свежий бинарник в ESP
 cp target/x86_64-unknown-none/debug/vados esp/EFI/vados
 
-# Запускаем QEMU
 qemu-system-x86_64 \
     -M q35 \
     -m 256M \
-    -bios /opt/homebrew/opt/qemu/share/qemu/edk2-x86_64-code.fd \
+    -drive if=pflash,format=raw,readonly=on,file=/opt/homebrew/Cellar/qemu/11.0.0/share/qemu/edk2-x86_64-code.fd \
     -drive format=raw,media=disk,file=fat:rw:esp \
-    -serial stdio \
-    -display none
+    -serial stdio
